ab017c4a108a465b52b3273b0ae37c0b85f9cb3b
Tarek Abdel Sater <tarek.g.abdelsater@gmail.com>
2024-09-23 23:23:03 +0400

    symlink to unify terraform provider versions

--- terraform/aws/_tfstate/versions.tf
+++ terraform/aws/_tfstate/versions.tf
@@ -1,18 +1,1 @@
-terraform {
-  required_version = "~> 1.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.0"
-    }
-  }
-}
+/Users/tot/wrkspc/interviews/oiai-challenge/terraform/aws/_common/versions.tf
